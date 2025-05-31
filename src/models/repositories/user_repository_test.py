75d255c0bb0c0de446715353052a95e31545b579
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-05-31 11:39:07 -0300

    test: started create tests

--- src/models/repositories/user_repository_test.py
+++ src/models/repositories/user_repository_test.py
@@ -1,14 +1,30 @@
 from .user_repository import UserRepository
 from src.models.settings.db_connection_handler import db_connection_handler
+from unittest.mock import Mock
+
+class MockCursor:
+    def __init__(self):
+        self.execute = Mock()
+        self.fetchone = Mock()
+
+class MockConnection:
+    def __init__(self):
+        self.cursor = Mock(return_value=MockCursor())
+        self.commit = Mock()
+
 
 def test_repository():
-    db_connection_handler.connect()
-    conn = db_connection_handler.get_connection()
-    repo = UserRepository(conn)
+    username = "jhoe"
+    password = "Teste!1@"
 
-    username = "bob"
-    password = "123Teste*"
+    mock_connection = MockConnection()
+    repo = UserRepository(mock_connection)
 
-    user = repo.get_user_by_username(username)
-    print()
-    print(user)
+    repo.registry_user(username, password)
+
+    cursor = mock_connection.cursor.return_value
+
+    assert "INSERT INTO users " in cursor.execute.call_args[0][0]
+    assert "(username, password, balance)" in cursor.execute.call_args[0][0]
+    assert "VALUES" in cursor.execute.call_args[0][0]
+    assert cursor.execute.call_args[0][1] == (username, password, 0)
