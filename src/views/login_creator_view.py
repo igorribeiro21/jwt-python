72ea38eb0bcb7a12ce4cb8f3a014f1d678f784af
igor.ribeiro <igorapalvesribeiro@gmail.com>
2025-06-24 21:26:16 -0300

    feat: implemented BalanceEditorView and LoginCreatorView

--- src/views/login_creator_view.py
+++ src/views/login_creator_view.py
@@ -1,10 +1,10 @@
-from src.controllers.interfaces.user_register import UserRegisterInterface
+from src.controllers.interfaces.login_creator import LoginCreatorInterface
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 from .interfaces.view_interface import ViewInterface
 
-class UserRegisterView(ViewInterface):
-    def __init__(self, controller: UserRegisterInterface):
+class LoginCreatorView(ViewInterface):
+    def __init__(self, controller: LoginCreatorInterface):
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
@@ -12,9 +12,9 @@ class UserRegisterView(ViewInterface):
         password = http_request.body.get("password")
         self.__validate_inputs(username, password)
 
-        response = self.__controller.registry(username, password)
+        response = self.__controller.create(username, password)
 
-        return HttpResponse(body = { "data": response }, status_code = 201)
+        return HttpResponse(body = { "data": response }, status_code = 200)
 
     def __validate_inputs(self, username: any, password: any) -> None:
         if (
